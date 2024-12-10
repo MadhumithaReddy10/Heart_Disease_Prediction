6807e82f81107f64cca4096781bc6c142e18d20e
MadhumithaReddy10 <128881733+MadhumithaReddy10@users.noreply.github.com>
2024-12-10 13:54:54 +0530

    Update models.py

--- users/models.py
+++ users/models.py
@@ -4,14 +4,12 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    phone_number = models.CharField(max_length=15)
-    dob = models.DateField(null=True, blank=True)
+    phone_number=models.CharField(max_length=15)
+    dob=models.DateField(null=True,blank=True)
+    hospital_name=models.CharField(blank = True,max_length = 100)
+
+   
+    def __str__(self):
+        return self.user.username
 
 
-class Patient(models.Model):
-    name = models.CharField(max_length=100)
-    age = models.IntegerField()
-    phn_number = models.CharField(max_length=15)
-    blood_pressure = models.CharField(max_length=10)
-    sugar_levels = models.CharField(max_length=10)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
