631f383b39b3e74903d1b9655034d8284fe40c94
MadhumithaReddy10 <128881733+MadhumithaReddy10@users.noreply.github.com>
2024-12-12 16:47:13 +0530

    Delete users directory

--- users/utils.py
+++
@@ -1,9 +0,0 @@
-# your_app/utils.py
-from django.core.mail import send_mail
-from django.conf import settings
-
-def send_otp_email(user_email, otp):
-    subject = 'Your OTP Code'
-    message = f'Your OTP code is: {otp}'
-    from_email = settings.DEFAULT_FROM_EMAIL
-    send_mail(subject, message, from_email, [user_email])
